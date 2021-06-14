328a4f6ef0741a8b32ff7ca412fbfaad982dde00
shimanbb <shimanbb@gmail.com>
2021-06-15 00:02:22 +1000

    JSX fragment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  <h1 style={{color: "blue"}}>Home</h1>,
+  <>
+  <h1 style={{color: "blue"}}>Home</h1>
+  <h2>This is a header 2</h2>
+  </>,
   document.getElementById("root")
 );
