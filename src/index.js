413d54b8bc64e8c221b30de0983b823a8633fcac
shimanbb <shimanbb@gmail.com>
2021-06-15 00:01:26 +1000

    Passing properties

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", null, "Home"), 
+  React.createElement("h1", {style: {color: "red"}}, "Home"),
   document.getElementById("root")
 );
